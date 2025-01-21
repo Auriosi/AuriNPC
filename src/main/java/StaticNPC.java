440de882e6794de73a73b5daab05587fdfc38cc2
Auriosi <email@email.com>
2025-01-21 10:39:59 -0600

    Added lookRange and tick functionality for StaticNPC

--- src/main/java/StaticNPC.java
+++ src/main/java/StaticNPC.java
@@ -1,14 +1,12 @@
 import net.kyori.adventure.text.Component;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.PlayerSkin;
+import net.minestom.server.entity.*;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.network.packet.server.play.PlayerInfoUpdatePacket;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,17 +16,19 @@ public class StaticNPC extends Entity implements NPC {
     private String skinValue;
     private boolean lookAtPlayers;
     private boolean listed;
+    private long lookRangeSquared;
 
     private PlayerInfoUpdatePacket playerInfoUpdatePacket;
 
     private StaticNPC(
         @NotNull UUID uuid,
         @NotNull Pos position,
-        Component customName,
-        String skinSignature,
-        String skinValue,
+        @NotNull Component customName,
+        @NotNull String skinSignature,
+        @NotNull String skinValue,
         boolean lookAtPlayers,
-        boolean listed
+        boolean listed,
+        long lookRange
     ) {
         // Constructor for full initial customization
         super(EntityType.PLAYER, uuid);
@@ -37,6 +37,7 @@ public class StaticNPC extends Entity implements NPC {
         this.skinValue = skinValue;
         this.lookAtPlayers = lookAtPlayers;
         this.listed = listed;
+        this.lookRangeSquared = lookRange;
         this.setCustomName(customName);
         this.setCustomNameVisible(true);
         this.playerInfoUpdatePacket = new PlayerInfoUpdatePacket(
@@ -68,14 +69,15 @@ public class StaticNPC extends Entity implements NPC {
         private String skinValue = "";
         private boolean lookAtPlayers = true;
         private boolean listed = true;
+        private long lookRange = 10;
 
-        public Builder(UUID uuid, Pos position) {
+        public Builder(@NotNull UUID uuid, @NotNull Pos position) {
             this.uuid = uuid;
             this.position = position;
         }
 
         public StaticNPC build() {
-            return new StaticNPC(uuid, position, customName, skinSignature, skinValue, lookAtPlayers, listed);
+            return new StaticNPC(uuid, position, customName, skinSignature, skinValue, lookAtPlayers, listed, lookRange);
         }
 
         public Builder customName(Component customName) {
@@ -104,14 +106,37 @@ public class StaticNPC extends Entity implements NPC {
             this.listed = listed;
             return this;
         }
+
+        public Builder lookRange(long lookRange) {
+            this.lookRange = lookRange;
+            return this;
+        }
+    }
+
+    @Override
+    public void tick(long time) {
+        if (lookAtPlayers) {
+            Player closest = null;
+            double closestDistance = Double.MAX_VALUE;
+            for (Entity nearbyEntity : getInstance().getNearbyEntities(this.position, this.lookRangeSquared)) {
+                if (nearbyEntity instanceof Player) {
+                    double distance = this.position.distance(nearbyEntity.getPosition());
+                    if (distance < closestDistance) {
+                        closest = (Player) nearbyEntity;
+                        closestDistance = distance;
+                    }
+                }
+            }
+            if (closest != null) {
+                this.lookAt(closest.getPosition());
+            }
+        }
     }
 
     @Override
     public CompletableFuture<Void> setInstance(@NotNull Instance instance) {
         CompletableFuture<Void> future = super.setInstance(instance);
-        future.thenRun(() -> {
-            AuriNPC.getInstance().addNPC(this);
-        });
+        future.thenRun(() -> AuriNPC.getInstance().addNPC(this));
         return future;
     }
 
@@ -124,7 +149,7 @@ public class StaticNPC extends Entity implements NPC {
             PlayerInfoUpdatePacket.Action.ADD_PLAYER,
             new PlayerInfoUpdatePacket.Entry(
                 getUuid(),
-                getCustomName().examinableName(),
+                Objects.requireNonNull(getCustomName()).toString(),
                 List.of(
                         new PlayerInfoUpdatePacket.Property("textures", skinValue, skinSignature) // The client should be able to handle a blank skin property
                 ),
@@ -136,9 +161,7 @@ public class StaticNPC extends Entity implements NPC {
                 0
             )
         );
-        getInstance().getPlayers().forEach(player -> {
-            player.sendPacket(playerInfoUpdatePacket);
-        });
+        getInstance().getPlayers().forEach(player -> player.sendPacket(playerInfoUpdatePacket));
     }
 
     public String getSkinSignature() {
@@ -149,7 +172,7 @@ public class StaticNPC extends Entity implements NPC {
         return skinValue;
     }
 
-    public void setSkin(String skinValue, String skinSignature) {
+    public void setSkin(@NotNull String skinValue, @NotNull String skinSignature) {
         this.skinValue = skinValue;
         this.skinSignature = skinSignature;
         remakeInfoUpdatePacket();
@@ -174,5 +197,17 @@ public class StaticNPC extends Entity implements NPC {
     public void setListed(boolean listed) {
         this.listed = listed;
         remakeInfoUpdatePacket();
+    }
+
+    public long getLookRangeSquared() {
+        return lookRangeSquared;
+    }
+
+    public long getLookRange() {
+        return (long) Math.sqrt(lookRangeSquared);
+    }
+
+    public void setLookRange(long lookRange) {
+        this.lookRangeSquared = lookRange * lookRange;
     }
 }
