2647107e6bc7c95cec45bf4415f32a0bccaef9f7
Auriosi <email@email.com>
2025-01-21 10:52:53 -0600

    Initial creation of LivingStaticNPC

--- src/main/java/LivingStaticNPC.java
+++ src/main/java/LivingStaticNPC.java
@@ -10,24 +10,34 @@ import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-public class StaticNPC extends Entity implements NPC {
+public class LivingStaticNPC extends LivingEntity implements NPC {
     private String skinSignature;
     private String skinValue;
     private boolean lookAtPlayers;
     private boolean listed;
     private long lookRangeSquared;
+    private double maxHealth;
+    private double health;
+    private boolean invulnerable;
+    private boolean respawns;
+    private long respawnDelay;
 
     private PlayerInfoUpdatePacket playerInfoUpdatePacket;
 
-    protected StaticNPC(
-        @NotNull UUID uuid,
-        @NotNull Pos position,
-        @NotNull Component customName,
-        @NotNull String skinSignature,
-        @NotNull String skinValue,
-        boolean lookAtPlayers,
-        boolean listed,
-        long lookRange
+    protected LivingStaticNPC(
+            @NotNull UUID uuid,
+            @NotNull Pos position,
+            @NotNull Component customName,
+            @NotNull String skinSignature,
+            @NotNull String skinValue,
+            boolean lookAtPlayers,
+            boolean listed,
+            long lookRange,
+            double maxHealth,
+            double health,
+            boolean invulnerable,
+            boolean respawns,
+            long respawnDelay
     ) {
         // Constructor for full initial customization
         super(EntityType.PLAYER, uuid);
@@ -37,23 +47,28 @@ public class StaticNPC extends Entity implements NPC {
         this.lookAtPlayers = lookAtPlayers;
         this.listed = listed;
         this.lookRangeSquared = lookRange;
+        this.maxHealth = maxHealth;
+        this.health = health;
+        this.invulnerable = invulnerable;
+        this.respawns = respawns;
+        this.respawnDelay = respawnDelay;
         this.setCustomName(customName);
         this.setCustomNameVisible(true);
         this.playerInfoUpdatePacket = new PlayerInfoUpdatePacket(
-            PlayerInfoUpdatePacket.Action.ADD_PLAYER,
-            new PlayerInfoUpdatePacket.Entry(
-                uuid,
-                customName.examinableName(),
-                List.of(
-                    new PlayerInfoUpdatePacket.Property("textures", skinValue, skinSignature) // The client should be able to handle a blank skin property
-                ),
-                this.listed,
-                0,
-                GameMode.CREATIVE,
-                customName,
-                null,
-                0
-            )
+                PlayerInfoUpdatePacket.Action.ADD_PLAYER,
+                new PlayerInfoUpdatePacket.Entry(
+                        uuid,
+                        customName.examinableName(),
+                        List.of(
+                                new PlayerInfoUpdatePacket.Property("textures", skinValue, skinSignature) // The client should be able to handle a blank skin property
+                        ),
+                        this.listed,
+                        0,
+                        GameMode.CREATIVE,
+                        customName,
+                        null,
+                        0
+                )
         );
     }
 
@@ -61,6 +76,7 @@ public class StaticNPC extends Entity implements NPC {
         // Required parameters
         private final UUID uuid;
         private final Pos position;
+        private final double maxHealth;
 
         // Optional parameters
         private Component customName = Component.text("NPC");
@@ -69,45 +85,70 @@ public class StaticNPC extends Entity implements NPC {
         private boolean lookAtPlayers = true;
         private boolean listed = true;
         private long lookRange = 10;
+        private double health = 1;
+        private boolean invulnerable = false;
+        private boolean respawns = false;
+        private long respawnDelay = 0;
 
-        public Builder(@NotNull UUID uuid, @NotNull Pos position) {
+        public Builder(@NotNull UUID uuid, @NotNull Pos position, double maxHealth) {
             this.uuid = uuid;
             this.position = position;
+            this.maxHealth = maxHealth;
         }
 
-        public StaticNPC build() {
-            return new StaticNPC(uuid, position, customName, skinSignature, skinValue, lookAtPlayers, listed, lookRange);
+        public LivingStaticNPC build() {
+            return new LivingStaticNPC(uuid, position, customName, skinSignature, skinValue, lookAtPlayers, listed, lookRange, maxHealth, health, invulnerable, respawns, respawnDelay);
         }
 
-        public StaticNPC.Builder customName(Component customName) {
+        public LivingStaticNPC.Builder customName(Component customName) {
             this.customName = customName;
             return this;
         }
 
-        public StaticNPC.Builder skin(String skinValue, String skinSignature) {
+        public LivingStaticNPC.Builder skin(String skinValue, String skinSignature) {
             this.skinValue = skinValue;
             this.skinSignature = skinSignature;
             return this;
         }
 
-        public StaticNPC.Builder skin(PlayerSkin skin) {
+        public LivingStaticNPC.Builder skin(PlayerSkin skin) {
             this.skinValue = skin.textures();
             this.skinSignature = skin.signature();
             return this;
         }
 
-        public StaticNPC.Builder lookAtPlayers(boolean lookAtPlayers) {
+        public LivingStaticNPC.Builder lookAtPlayers(boolean lookAtPlayers) {
             this.lookAtPlayers = lookAtPlayers;
             return this;
         }
 
-        public StaticNPC.Builder listed(boolean listed) {
+        public LivingStaticNPC.Builder listed(boolean listed) {
             this.listed = listed;
             return this;
         }
 
-        public StaticNPC.Builder lookRange(long lookRange) {
+        public LivingStaticNPC.Builder lookRange(long lookRange) {
             this.lookRange = lookRange;
+            return this;
+        }
+
+        public LivingStaticNPC.Builder health(double health) {
+            this.health = health;
+            return this;
+        }
+
+        public LivingStaticNPC.Builder invulnerable(boolean invulnerable) {
+            this.invulnerable = invulnerable;
+            return this;
+        }
+
+        public LivingStaticNPC.Builder respawns(boolean respawns) {
+            this.respawns = respawns;
+            return this;
+        }
+
+        public LivingStaticNPC.Builder respawnDelay(long respawnDelay) {
+            this.respawnDelay = respawnDelay;
             return this;
         }
     }
@@ -145,20 +186,20 @@ public class StaticNPC extends Entity implements NPC {
 
     public void remakeInfoUpdatePacket() {
         this.playerInfoUpdatePacket = new PlayerInfoUpdatePacket(
-            PlayerInfoUpdatePacket.Action.ADD_PLAYER,
-            new PlayerInfoUpdatePacket.Entry(
-                getUuid(),
-                Objects.requireNonNull(getCustomName()).toString(),
-                List.of(
-                        new PlayerInfoUpdatePacket.Property("textures", skinValue, skinSignature) // The client should be able to handle a blank skin property
-                ),
-                this.listed,
-                0,
-                GameMode.CREATIVE,
-                getCustomName(),
-                null,
-                0
-            )
+                PlayerInfoUpdatePacket.Action.ADD_PLAYER,
+                new PlayerInfoUpdatePacket.Entry(
+                        getUuid(),
+                        Objects.requireNonNull(getCustomName()).toString(),
+                        List.of(
+                                new PlayerInfoUpdatePacket.Property("textures", skinValue, skinSignature) // The client should be able to handle a blank skin property
+                        ),
+                        this.listed,
+                        0,
+                        GameMode.CREATIVE,
+                        getCustomName(),
+                        null,
+                        0
+                )
         );
         getInstance().getPlayers().forEach(player -> player.sendPacket(playerInfoUpdatePacket));
     }
@@ -208,5 +249,9 @@ public class StaticNPC extends Entity implements NPC {
 
     public void setLookRange(long lookRange) {
         this.lookRangeSquared = lookRange * lookRange;
+    }
+
+    public void setMaxHealth(double maxHealth) {
+        this.maxHealth = maxHealth;
     }
 }
